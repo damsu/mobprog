0634f20a53c74ac6a8a6bdf9a1704e110a0f7b4e
damsu <damorizur@gmail.com>
2016-09-11 14:51:56 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-//var books = require('./routes/books.js');
-//var authors = require('./routes/authors.js');
-var students = require('./routes/students.js');
-var courses = require('./routes/courses.js');
-//var grades = require('./routes/grades.js');
+var books = require('./routes/books.js');
+var authors = require('./routes/authors.js');
 var app = express();
 
 // You can store key-value pairs in express, here we store the port setting
@@ -15,11 +12,8 @@ app.set('port', (process.env.PORT || 80));
 app.use(bodyParser.json());
 
 // Mount our routes behind /api/ prefix
-//app.use('/api', books);
-//app.use('/api', authors);
-app.use('/api', students);
-app.use('/api', courses);
-//app.use('/api', grades);
+app.use('/api', books);
+app.use('/api', authors);
 
 // Simple hello world route
 app.get('/', function(req, res) {
