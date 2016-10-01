2c120468297cb3730e867e1c61b3f484a065a0ef
damsu <damorizur@gmail.com>
2016-10-01 18:53:09 +0300

    Add files via upload

--- src/data.js
+++ src/data.js
@@ -16,7 +16,7 @@ var studentData = [{
         id: 0,
         username: 'Ben Sparrow',
         userId: 0,
-        comments: 'You on your way?',
+        caption: 'You on your way?',
         face: 'http://kurld.com/images/wallpapers/profile-pictures/profile-pictures-14.jpg',
         pic: 'http://wac.450f.edgecastcdn.net/80450F/tri1025.com/files/2013/05/normal_me1.jpg',
         likes: 3
@@ -24,7 +24,7 @@ var studentData = [{
         id: 1,
         username: 'damien',
         userId: 5,
-        comments: 'Hey, it\'s me',
+        caption: 'Hey, it\'s me',
         face: 'https://scontent-ams3-1.xx.fbcdn.net/v/t1.0-9/10502074_10153145101446416_4071352603442079179_n.jpg?oh=72e67ed294c0bbb6c1bc585be5bf2c1b&oe=58654897',
         pic: 'http://under30ceo.com/wp-content/uploads/2012/04/instagram.jpg',
         likes: 19
@@ -32,7 +32,7 @@ var studentData = [{
         id: 2,
         username: 'Ben Sparrow',
         userId: 0,
-        comments: 'I should buy a boat',
+        caption: 'I should buy a boat',
         face: 'http://kurld.com/images/wallpapers/profile-pictures/profile-pictures-14.jpg',
         pic: 'https://s-media-cache-ak0.pinimg.com/736x/1e/93/28/1e9328a87eb6654e3a290645a9620c9d.jpg',
         likes: 5
@@ -40,7 +40,7 @@ var studentData = [{
         id: 3,
         username: 'Ben Sparrow',
         userId: 0,
-        comments: 'Look at my mukluks!',
+        caption: 'Look at my mukluks!',
         face: 'http://kurld.com/images/wallpapers/profile-pictures/profile-pictures-14.jpg',
         pic: 'http://4.bp.blogspot.com/-F_6SfcFHKRE/UIjJKWfbt8I/AAAAAAAAA6w/AK5H_oGl9io/s1600/nature182.jpg',
         likes: 63
@@ -48,7 +48,7 @@ var studentData = [{
         id: 4,
         username: 'damien',
         userId: 5,
-        comments: 'This is wicked good ice cream.',
+        caption: 'This is wicked good ice cream.',
         face: 'https://scontent-ams3-1.xx.fbcdn.net/v/t1.0-9/10502074_10153145101446416_4071352603442079179_n.jpg?oh=72e67ed294c0bbb6c1bc585be5bf2c1b&oe=58654897',
         pic: 'http://imgs.abduzeedo.com/files/paul0v2/livefolk/livefolk-03.jpg',
         likes: 11
@@ -56,7 +56,7 @@ var studentData = [{
         id: 5,
         username: 'damien',
         userId: 5,
-        comments: 'This is wicked good ice cream.',
+        caption: 'This is wicked good ice cream.',
         face: 'https://scontent-ams3-1.xx.fbcdn.net/v/t1.0-9/10502074_10153145101446416_4071352603442079179_n.jpg?oh=72e67ed294c0bbb6c1bc585be5bf2c1b&oe=58654897',
         pic: 'https://katepattinson1994.files.wordpress.com/2014/04/free-people-2.jpg',
         likes: 11
@@ -64,7 +64,7 @@ var studentData = [{
         id: 6,
         username: 'Ben Sparrow',
         userId: 0,
-        comments: 'This is wicked good ice cream.',
+        caption: 'This is wicked good ice cream.',
         face: 'http://kurld.com/images/wallpapers/profile-pictures/profile-pictures-14.jpg',
         pic: 'http://thetechpanda.com/wp-content/uploads/2012/07/instagram.jpg',
         likes: 11
@@ -72,7 +72,7 @@ var studentData = [{
         id: 7,
         username: 'damien',
         userId: 5,
-        comments: 'This is wicked good ice cream.',
+        caption: 'This is wicked good ice cream.',
         face: 'https://scontent-ams3-1.xx.fbcdn.net/v/t1.0-9/10502074_10153145101446416_4071352603442079179_n.jpg?oh=72e67ed294c0bbb6c1bc585be5bf2c1b&oe=58654897',
         pic: 'http://www.telegraph.co.uk/content/dam/fashion/DEC/Dec3/donatella-gigi-lead-large.jpg',
         likes: 11
@@ -80,7 +80,7 @@ var studentData = [{
         id: 8,
         username: 'Ben Sparrow',
         userId: 0,
-        comments: 'This is wicked good ice cream.',
+        caption: 'This is wicked good ice cream.',
         face: 'http://kurld.com/images/wallpapers/profile-pictures/profile-pictures-14.jpg',
         pic: 'http://cdn.rsvlts.com/wp-content/uploads/2014/01/48.-Bruno-Mars.jpg',
         likes: 11
@@ -88,7 +88,7 @@ var studentData = [{
         id: 9,
         username: 'Ben Sparrow',
         userId: 0,
-        comments: 'This is wicked good ice cream.',
+        caption: 'This is wicked good ice cream.',
         face: 'http://kurld.com/images/wallpapers/profile-pictures/profile-pictures-14.jpg',
         pic: 'http://thestylespy.com/wp/wp-content/uploads/3ee47de07e0711e3bbf50e7e7eef1a38_7.jpg',
         likes: 11
@@ -96,7 +96,7 @@ var studentData = [{
         id: 10,
         username: 'damien',
         userId: 5,
-        comments: 'This is wicked good ice cream.',
+        caption: 'This is wicked good ice cream.',
         face: 'https://scontent-ams3-1.xx.fbcdn.net/v/t1.0-9/10502074_10153145101446416_4071352603442079179_n.jpg?oh=72e67ed294c0bbb6c1bc585be5bf2c1b&oe=58654897',
         pic: 'http://data.whicdn.com/images/60119606/large.jpg',
         likes: 11
@@ -104,7 +104,7 @@ var studentData = [{
         id: 11,
         username: 'Ben Sparrow',
         userId: 0,
-        comments: 'This is wicked good ice cream.',
+        caption: 'This is wicked good ice cream.',
         face: 'http://kurld.com/images/wallpapers/profile-pictures/profile-pictures-14.jpg',
         pic: 'http://data.whicdn.com/images/63547742/large.jpg',
         likes: 11
@@ -112,7 +112,7 @@ var studentData = [{
         id: 12,
         username: 'damien',
         userId: 5,
-        comments: 'This is wicked good ice cream.',
+        caption: 'This is wicked good ice cream.',
         face: 'https://scontent-ams3-1.xx.fbcdn.net/v/t1.0-9/10502074_10153145101446416_4071352603442079179_n.jpg?oh=72e67ed294c0bbb6c1bc585be5bf2c1b&oe=58654897',
         pic: 'http://i.dailymail.co.uk/i/pix/2015/12/11/19/2F49AD8700000578-3356493-image-a-2_1449860985376.jpg',
         likes: 11
@@ -120,7 +120,7 @@ var studentData = [{
         id: 13,
         username: 'damien',
         userId: 5,
-        comments: 'This is wicked good ice cream.',
+        caption: 'This is wicked good ice cream.',
         face: 'https://scontent-ams3-1.xx.fbcdn.net/v/t1.0-9/10502074_10153145101446416_4071352603442079179_n.jpg?oh=72e67ed294c0bbb6c1bc585be5bf2c1b&oe=58654897',
         pic: 'http://sizlingpeople.com/wp-content/uploads/2016/02/Kendall-Jenner-Instagram.jpg',
         likes: 11
@@ -128,7 +128,7 @@ var studentData = [{
         id: 14,
         username: 'damien',
         userId: 5,
-        comments: 'This is wicked good ice cream.',
+        caption: 'This is wicked good ice cream.',
         face: 'https://scontent-ams3-1.xx.fbcdn.net/v/t1.0-9/10502074_10153145101446416_4071352603442079179_n.jpg?oh=72e67ed294c0bbb6c1bc585be5bf2c1b&oe=58654897',
         pic: 'https://ioneglobalgrind.files.wordpress.com/2012/10/instagram-lg.jpeg',
         likes: 11
